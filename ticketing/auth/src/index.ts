ed3b1c9f3a84d86083178b327ad4065a941949af
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-08 00:29:06 +0800

    Set up error middleware and connect to mongo db

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,5 +1,15 @@
 import express from 'express'
+import 'express-async-errors'
+import mongoose from 'mongoose'
 import { json, urlencoded } from 'body-parser'
+
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
+
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const PORT = 3000
 const app = express()
@@ -7,10 +17,36 @@ const app = express()
 app.use(json())
 app.use(urlencoded({ extended: true }))
 
-app.get('/api/users/currentuser', (req, res) => {
-  return res.status(200).send('It is done!!!')
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
+
+// Handle 404 errors
+app.all('*', () => {
+  throw new NotFoundError()
 })
 
-app.listen(PORT, () => {
-  console.log(`App is listening at port ${PORT}`)
-})
+// Attach error handling middleware
+app.use(errorHandler)
+
+const start = async () => {
+  try {
+    // Connect to MongoDB instance on service
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    console.log('Connected to MongoDB!')
+  } catch (err) {
+    console.error(err)
+  }
+
+  // Listen to port
+  app.listen(PORT, () => {
+    console.log(`App is listening at port ${PORT}!`)
+  })
+}
+
+start()
