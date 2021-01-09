45603cfb218544c28928679d15397ba20d22eb9a
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-09 17:27:41 +0800

    Add current-user route and middleware

---
+++ ticketing/auth/src/middlewares/current-user.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+interface UserPayload {
+  id: string;
+  email: string
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload // An optional currentUser field for request
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
+  // If no cookie or no json web token
+  if (!req.session || !req.session.jwt) {
+    return next()
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload
+
+    req.currentUser = payload
+    
+    return res.status(200).send({ currentUser: payload })
+  } catch {
+    // If token validation fails
+    return next()
+  }
+}
