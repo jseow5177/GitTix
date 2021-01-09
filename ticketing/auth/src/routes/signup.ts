87db0c912fc494abd3e31fe718456da0e68e4be3
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-09 12:45:30 +0800

    Move password hashing logic into signup route

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
+
+import { Password } from '../utils/password'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { BadRequestError } from '../errors/bad-request-error'
+import { UserSaveError } from '../errors/user-save-error'
 import { User } from '../models/user'
 
 const router = express.Router()
@@ -26,16 +30,43 @@ router.post('/api/users/signup',
 
     const { email, password } = req.body
 
+    // Check with a user with the sign up email already exist
     const existingUser = await User.findOne({ email })
 
     if (existingUser) {
       throw new BadRequestError('Email already in use')
     }
 
-    const user = User.build({ email, password })
-    await user.save()
+    try {
+      // Hash password with scrypt
+      const hashedPassword = await Password.toHash(password)
 
-    return res.status(201).send(user)
+      const user = User.build({ email, password: hashedPassword })
+      
+      // Save user into DB
+      await user.save()
+
+      // Generate JWT (Synchronous)
+      const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+      },
+        // Append a non-null assertion operator (!)
+        // Tells the TypeScript compiler that this expression can never be null or undefined
+        // as a check is already made at app startup
+        process.env.JWT_KEY!
+      )
+
+      // Store in session object
+      req.session = {
+        jwt: userJwt
+      }
+
+      // user document will be converted to JSON (through the toJSON method) and returned
+      return res.status(201).send(user)
+    } catch {
+      throw new UserSaveError('Sign up failed')
+    }
 })
 
 export { router as signupRouter }
