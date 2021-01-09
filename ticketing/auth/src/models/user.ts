c422aa1077527215286a54f2b248a6b2f5792284
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-09 12:43:53 +0800

    Modify user document toJSON method

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose'
-import { Password } from '../utils/password'
 
 // An interface that describes the properties
 // that are required to create a new User.
 interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Document (a single user) has
+interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
@@ -14,34 +20,39 @@ interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttrs): UserDoc;
 }
 
-// An interface that describes the properties
-// that a User Document (a single user) has
-interface UserDoc extends mongoose.Document {
-  email: string;
-  password: string;
-}
-
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
   },
-  password: {
-    type: String,
-    required: true
+  {
+    /**
+     * Called when document object is converted to JSON.
+     * This is to standardize the JSON properties of returned user.
+     * 
+     * It is not common to place such logic on the model level.
+     */
+    toJSON: {
+      transform(doc, ret) {
+        /**
+         * @param doc The mongoose document which is being converted
+         * @param ret The plain object representation which has been converted
+         * 
+         * We will directly modify the object being returned
+         */
+        ret.id = ret._id // Remap _id field to id
+        delete ret._id // Remove _id field
+        delete ret.password // Remove password field
+      }
+    },
+    versionKey: false // Remove version key (__v)
   }
-})
-
-// pre-save hook
-userSchema.pre('save', async function (done) {
-  // Check if the password field is modified
-  // When a user is first created, the password field is considered as being modified as well
-  if (this.isModified('password')) {
-    const hashed = await Password.toHash(this.get('password'))
-    this.set('password', hashed)
-  }
-  done() // Indicates that the async work is done
-})
+)
 
 // static property to build user
 userSchema.statics.build = (attrs: UserAttrs) => {
