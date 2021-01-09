97d02aa4920ae4b8ff7d720ba1174d0389e09485
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-09 12:44:34 +0800

    Wrap scrypt hashing in try/catch block

--- ticketing/auth/src/utils/password.ts
+++ ticketing/auth/src/utils/password.ts
@@ -15,20 +15,28 @@ export class Password {
     // Generate a random salt of 16 bytes to hash password
     const salt = randomBytes(16).toString('hex')
 
-    // Hash the password with salt. The hash is returned as a Buffer with 64 bytes.
-    // Need to tell TypeScript that buffer is of type Buffer.
-    const buffer = (await scryptAsync(password, salt, 64)) as Buffer
-  
-    // Convert buffer to string and concatenate it with the salt
-    return `${buffer.toString('hex')}.${salt}`
+    try {
+      // Hash the password with salt. The hash is returned as a Buffer with 64 bytes.
+      // Need to tell TypeScript that buffer is of type Buffer.
+      const buffer = (await scryptAsync(password, salt, 64)) as Buffer
+
+      // Convert buffer to string and concatenate it with the salt
+      return `${buffer.toString('hex')}.${salt}`
+    } catch (error) {
+      throw error
+    }
   }
 
   static async compare(storedPassword: string, suppliedPassword: string) {
     const [hashedPassword, salt] = storedPassword.split('.')
 
-    const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer
+    try {
+      const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer
 
-    // Compare hashes
-    return buffer.toString('hex') === hashedPassword
+      // Compare hashes
+      return buffer.toString('hex') === hashedPassword
+    } catch (error) {
+      throw error
+    }
   }
 }
