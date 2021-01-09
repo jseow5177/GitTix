73f7c783cf17863dc65712c5415e27124b9bfb1b
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-09 12:43:17 +0800

    Set up cookie session

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
+import 'express-async-errors' // Allow async errors to use throw instead of passing into next()
 import express from 'express'
-import 'express-async-errors'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
 import { json, urlencoded } from 'body-parser'
 
 import { currentUserRouter } from './routes/current-user'
@@ -14,8 +15,20 @@ import { NotFoundError } from './errors/not-found-error'
 const PORT = 3000
 const app = express()
 
-app.use(json())
-app.use(urlencoded({ extended: true }))
+// A app configuration for Express server that is running behind a proxy (in this case, ingress-nginx)
+// Tells Express to trust the traffic forwarded by the proxy
+// https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', true)
+
+app.use(json()) // Parse json data
+app.use(urlencoded({ extended: true })) // Parse url params with the qs library
+
+app.use(
+  cookieSession({
+    signed: false, // cookie content will not be encrypted
+    secure: true // cookie is only to be sent over HTTPS
+  })
+)
 
 app.use(currentUserRouter)
 app.use(signinRouter)
@@ -31,6 +44,11 @@ app.all('*', () => {
 app.use(errorHandler)
 
 const start = async () => {
+  // Ensure that the JWT_KEY is present in the environment variable before starting the application
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT secret key is not defined')
+  }
+
   try {
     // Connect to MongoDB instance on service
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
