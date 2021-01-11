561c2e353e0fc76fe5686fe9a339ae7efe94273f
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-11 09:59:16 +0800

    Refactor to pull app out of index.js

---
+++ ticketing/auth/src/app.ts
@@ -0,0 +1,44 @@
+import 'express-async-errors' // Allow async errors to use throw instead of passing into next()
+import express from 'express'
+import cookieSession from 'cookie-session'
+import { json, urlencoded } from 'body-parser'
+
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
+
+import { errorHandler } from './middlewares/error-handler'
+import { AuthServiceError } from './errors/auth-service-error'
+
+const app = express()
+
+// A app configuration for Express server that is running behind a proxy (in this case, ingress-nginx)
+// Tells Express to trust the traffic forwarded by the proxy
+// https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', true)
+
+app.use(json()) // Parse json data
+app.use(urlencoded({ extended: true })) // Parse url params with the qs library
+
+app.use(
+  cookieSession({
+    signed: false, // cookie content will not be encrypted
+    secure: true // cookie is only to be sent over HTTPS
+  })
+)
+
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
+
+// Handle 404 errors
+app.all('*', () => {
+  throw new AuthServiceError(404, 'Resource not found')
+})
+
+// Attach error handling middleware
+app.use(errorHandler)
+
+export { app }
--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,47 +1,9 @@
-import 'express-async-errors' // Allow async errors to use throw instead of passing into next()
-import express from 'express'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-import { json, urlencoded } from 'body-parser'
 
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
-
-import { errorHandler } from './middlewares/error-handler'
+import { app } from './app'
 import { AuthServiceError } from './errors/auth-service-error'
 
 const PORT = 3000
-const app = express()
-
-// A app configuration for Express server that is running behind a proxy (in this case, ingress-nginx)
-// Tells Express to trust the traffic forwarded by the proxy
-// https://expressjs.com/en/guide/behind-proxies.html
-app.set('trust proxy', true)
-
-app.use(json()) // Parse json data
-app.use(urlencoded({ extended: true })) // Parse url params with the qs library
-
-app.use(
-  cookieSession({
-    signed: false, // cookie content will not be encrypted
-    secure: true // cookie is only to be sent over HTTPS
-  })
-)
-
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
-
-// Handle 404 errors
-app.all('*', () => {
-  throw new AuthServiceError(404, 'Resource not found')
-})
-
-// Attach error handling middleware
-app.use(errorHandler)
 
 const start = async () => {
   // Ensure that the JWT_KEY is present in the environment variable before starting the application
