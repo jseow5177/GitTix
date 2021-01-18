90ed9d8d96537362a32b6e9d4d18fa859dc78e8c
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-18 11:41:46 +0800

    Remove unnecessary try catch block

--- ticketing/auth/src/utils/password.ts
+++ ticketing/auth/src/utils/password.ts
@@ -15,28 +15,18 @@ export class Password {
     // Generate a random salt of 16 bytes to hash password
     const salt = randomBytes(16).toString('hex')
 
-    try {
-      // Hash the password with salt. The hash is returned as a Buffer with 64 bytes.
-      // Need to tell TypeScript that buffer is of type Buffer.
-      const buffer = (await scryptAsync(password, salt, 64)) as Buffer
-
-      // Convert buffer to string and concatenate it with the salt
-      return `${buffer.toString('hex')}.${salt}`
-    } catch (error) {
-      throw error
-    }
+    // Hash the password with salt. The hash is returned as a Buffer with 64 bytes.
+    // Need to tell TypeScript that buffer is of type Buffer.
+    const buffer = (await scryptAsync(password, salt, 64)) as Buffer
+    return `${buffer.toString('hex')}.${salt}`
   }
 
   static async compare(storedPassword: string, suppliedPassword: string) {
     const [hashedPassword, salt] = storedPassword.split('.')
 
-    try {
-      const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer
+    const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer
 
-      // Compare hashes
-      return buffer.toString('hex') === hashedPassword
-    } catch (error) {
-      throw error
-    }
+    // Compare hashes
+    return buffer.toString('hex') === hashedPassword
   }
 }
