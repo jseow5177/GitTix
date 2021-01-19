1e7dd24914b4fffa2e6fabaf2101554155063c20
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-19 14:40:13 +0800

    Add route to retrieve all tickets

---
+++ ticketing/tickets/src/routes/get-tickets.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import { Ticket } from '../models/ticket'
+
+const router = express.Router()
+
+router.get('/api/tickets', async (req, res) => {
+  // Get all tickets
+  const tickets = await Ticket.find({})
+
+  return res.status(200).send(tickets)
+})
+
+export { router as getTicketsRouter }
