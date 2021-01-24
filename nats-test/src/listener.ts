ad37ce213280f54e1f169432cab898d24acde03e
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-24 17:21:46 +0800

    Abstract listen logic

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from 'node-nats-streaming'
+import nats from 'node-nats-streaming'
 import { randomBytes } from 'crypto'
+import { TicketCreatedListener } from './events/ticket-created/ticket-created-listener'
 
 console.clear()
 
@@ -16,28 +17,9 @@ stan.on('connect', () => {
     process.exit()
   })
 
-  const options = stan.subscriptionOptions()
-    .setManualAckMode(true) // Requires the listener to manually acknowledge the receive of an event
-    .setDeliverAllAvailable()
-    .setDurableName('listener-service')
+  const listener = new TicketCreatedListener(stan)
 
-  let subscription = stan.subscribe('ticket:created', 'listenerQueueGroup', options)
-
-  subscription.on('message', (msg: Message) => {
-    console.log('Message received')
-    /**
-     * getSubject():  Returns the subject associated with this Message.
-     * getSequence(): Returns the sequence number of the message in the stream.
-     * getData():     Returns the data associated with the message payload. If the stanEncoding is not 
-     *                set to 'binary', a string is returned.
-     */
-    const data = msg.getData()
-    const sequence = msg.getSequence()
-    console.log(`Received event #${sequence} with data: ${data}`)
-
-    msg.ack()
-  })
-
+  listener.listen()
 })
 
 /**
