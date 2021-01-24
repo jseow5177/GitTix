06e6b53c5f7304040df632645d6533d3690f39e3
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-24 17:15:54 +0800

    Abstract hardcoded channel names

---
+++ nats-test/src/events/subjects.ts
@@ -0,0 +1,10 @@
+/**
+ * Enum is a TypeScript feature that allows developers to define a set of named constants.
+ * 
+ * Docs: https://www.typescriptlang.org/docs/handbook/enums.html
+ */
+
+export enum Subjects {
+  TicketCreated = 'ticket:created',
+  OrderUpdated = 'order:updated'
+}
