adacbab271edd1388a2a2328788f5d75441c4a8e
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-29 00:39:28 +0800

    Add route to fetch orders

--- ticketing/orders/src/routes/get-orders.ts
+++ ticketing/orders/src/routes/get-orders.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express'
+import { requireAuth } from '@gittix-js/common'
+import { Order } from '../models/order'
 
 const router = express.Router()
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-  return res.send({})
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id
+  }).populate('ticket')
+  
+  return res.status(200).send(orders)
 })
 
 export { router as getOrdersRouter }
