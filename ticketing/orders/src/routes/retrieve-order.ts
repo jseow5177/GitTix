fe4202f3314f9691faaf2a47789847d28cd2335b
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-29 00:39:06 +0800

    Add route to retrieve order

--- ticketing/orders/src/routes/retrieve-order.ts
+++ ticketing/orders/src/routes/retrieve-order.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from 'express'
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@gittix-js/common'
+import { Order } from '../models/order'
 
 const router = express.Router()
 
-router.get('/api/orders/:id', async (req: Request, res: Response) => {
-  return res.send({})
+router.get('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const order = await Order.findById(req.params.orderId).populate('ticket')
+
+  if (!order) {
+    throw new NotFoundError()
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError()
+  }
+
+  return res.status(200).send(order)
 })
 
 export { router as retrieveOrderRouter }
