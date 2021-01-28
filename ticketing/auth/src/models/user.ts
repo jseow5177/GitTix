14a06d4605f2e5418f6efea6276de49058d37a02
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-28 16:28:08 +0800

    Add comments and fix indentation

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,67 +1,81 @@
 import mongoose from 'mongoose'
 
-// An interface that describes the properties
-// that are required to create a new User.
+/**
+ * An interface that describes the properties
+ *  that are required to create a new User.
+ */
 interface UserAttrs {
   email: string;
   password: string;
 }
 
-// An interface that describes the properties
-// that a User Document (a single user) has
+/**
+ * We need a separate interface for UserAttrs and UserDoc because
+ *  UserAttrs are the set of attributes needed to create a User.
+ * However, underneath the hood, MongoDB will add some custom properties to the created
+ *  User Document.
+ * UserDoc is for us to expose those properties so that TypeScript will be aware of them.
+ */
+
+/**
+ * An interface that describes the properties 
+ *  that a User Document (a single user) has.
+ */
 interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
 
-// An interface that describes the properties
-// that a User Model has.
+/**
+ * An interface that describes the properties
+ *  that a User Model has. 
+ */
 interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttrs): UserDoc;
 }
 
 const userSchema = new mongoose.Schema({
-    email: {
-      type: String,
-      required: true
-    },
-    password: {
-      type: String,
-      required: true
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, {
+  /**
+   * Called when document object is converted to JSON.
+   * This is to standardize the JSON properties of returned user.
+   * 
+   * It is not common to place such logic on the model level.
+   */
+  toJSON: {
+    transform(doc, ret) {
+      /**
+       * @param doc The mongoose document which is being converted
+       * @param ret The plain object representation which has been converted
+       * 
+       * We will directly modify the object being returned
+       */
+      ret.id = ret._id // Remap _id field to id
+      delete ret._id // Remove _id field
+      delete ret.password // Remove password field
     }
   },
-  {
-    /**
-     * Called when document object is converted to JSON.
-     * This is to standardize the JSON properties of returned user.
-     * 
-     * It is not common to place such logic on the model level.
-     */
-    toJSON: {
-      transform(doc, ret) {
-        /**
-         * @param doc The mongoose document which is being converted
-         * @param ret The plain object representation which has been converted
-         * 
-         * We will directly modify the object being returned
-         */
-        ret.id = ret._id // Remap _id field to id
-        delete ret._id // Remove _id field
-        delete ret.password // Remove password field
-      }
-    },
-    versionKey: false // Remove version key (__v)
-  }
-)
+  versionKey: false // Remove version key (__v)
+})
 
 // static property to build user
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs)
 }
 
-// mongoose.model<U, T>
-// T is the generic type of the returned value. In this case, mongoose.model returns data of type UserModel.
-// U is the generic type of the document. In this case, it is UserDoc.
+/**
+ * mongoose.model<U, T>
+ * T is the generic type of the returned value. In this case, mongoose.model returns data of type UserModel.
+ * U is the generic type of the document. In this case, it is UserDoc.
+ */
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
 
 export { User }
--- ticketing/tickets/src/models/ticket.ts
+++ ticketing/tickets/src/models/ticket.ts
@@ -1,68 +1,87 @@
 import mongoose from 'mongoose'
 
-// An interface that describes the properties
-// that are required to create a new User.
+/**
+ * An interface that describes the properties
+ *  that are required to create a new Ticket. 
+ */
 interface TicketAttrs {
   title: string;
   price: number;
   userId: string;
 }
 
-// An interface that describes the properties
-// that a User Document (a single user) has
+/**
+ * We need a separate interface for TicketAttrs and TicketDoc because
+ *  TicketAttrs are the set of attributes needed to create a User.
+ * However, underneath the hood, MongoDB will add some custom properties to the created
+ *  Ticket Document.
+ * TicketDoc is for us to expose those properties so that TypeScript will be aware of them.
+ */
+
+/**
+ * An interface that describes the properties
+ *  that a Ticket Document (a single ticket) has.
+ */
 interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
   userId: string;
 }
 
+/**
+ * An interface that describes the properties
+ *  that a Ticket Model has. 
+ */
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
 
 const ticketSchema = new mongoose.Schema({
-    title: {
-      type: String,
-      required: true
-    },
-    price: {
-      type: Number,
-      required: true,
-      min: 0
-    },
-    userId: {
-      type: String,
-      required: true
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  userId: {
+    type: String,
+    required: true
+  }
+}, {
+  /**
+   * Called when document object is converted to JSON.
+   * This is to standardize the JSON properties of returned ticket.
+   * 
+   * It is not common to place such logic on the model level.
+   */
+  toJSON: {
+    transform(doc, ret) {
+      /**
+       * @param doc The mongoose document which is being converted
+       * @param ret The plain object representation which has been converted
+       * 
+       * We will directly modify the object being returned
+       */
+      ret.id = ret._id // Remap _id field to id
+      delete ret._id // Remove _id field
     }
   },
-  {
-    /**
-     * Called when document object is converted to JSON.
-     * This is to standardize the JSON properties of returned ticket.
-     * 
-     * It is not common to place such logic on the model level.
-     */
-    toJSON: {
-      transform(doc, ret) {
-        /**
-         * @param doc The mongoose document which is being converted
-         * @param ret The plain object representation which has been converted
-         * 
-         * We will directly modify the object being returned
-         */
-        ret.id = ret._id // Remap _id field to id
-        delete ret._id // Remove _id field
-      }
-    },
-    versionKey: false // Remove version key (__v)
-  }
-)
+  versionKey: false // Remove version key (__v)
+})
 
 // static property to build ticket
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs)
 }
 
+/**
+ * mongoose.model<U, T>
+ * T is the generic type of the returned value. In this case, mongoose.model returns data of type TicketModel.
+ * U is the generic type of the document. In this case, it is TicketDoc.
+ */
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema)
 
 export { Ticket }
