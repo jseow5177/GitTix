7e9b88d08af60b67dae40e1b456754524619b4ce
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-28 15:53:50 +0800

    Add comments

--- ticketing/common/src/events/base/subjects.ts
+++ ticketing/common/src/events/base/subjects.ts
@@ -2,6 +2,8 @@
  * Enum is a TypeScript feature that allows developers to define a set of named constants.
  * 
  * Docs: https://www.typescriptlang.org/docs/handbook/enums.html
+ * 
+ * The following enum defines the different channels that NATS has.
  */
 
 export enum Subjects {
