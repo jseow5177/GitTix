142eeccc716163d4b8c6e539b76bfa7e552ad47f
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-29 17:26:06 +0800

    Typo

--- ticketing/common/src/events/order/order-cancelled-event.ts
+++ ticketing/common/src/events/order/order-cancelled-event.ts
@@ -1,6 +1,6 @@
 import { Subjects } from '../base/subjects'
 
-export interface OrderCancelledEvenet {
+export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
   data: {
     id: string; // Order id
