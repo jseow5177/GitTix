5ddcee5ffe5bccc9336b5fb6e93fd2e8fffd6a4c
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-01 21:13:02 +0800

    Publish event on order cancel

--- ticketing/orders/src/routes/delete-order.ts
+++ ticketing/orders/src/routes/delete-order.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from 'express'
 import { Order } from '../models/order'
-import { requireAuth, OrderStatus, NotFoundError, NotAuthorizedError } from '@gittix-js/common'
+import { natsWrapper } from '../nats-wrapper'
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher'
+import {
+  requireAuth,
+  OrderStatus,
+  NotFoundError,
+  NotAuthorizedError
+} from '@gittix-js/common'
 
 const router = express.Router()
 
 router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
-  const order = await Order.findById(req.params.orderId)
+  const order = await Order.findById(req.params.orderId).populate('ticket')
 
   if (!order) {
     throw new NotFoundError()
@@ -22,7 +29,14 @@ router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Res
   order.status = OrderStatus.Cancelled
   await order.save()
 
-  // TODO: Publish an event Expiration and Payment services that an order is cancelled
+  // Publish an event Expiration and Payment services that an order is cancelled
+  const publisher = new OrderCancelledPublisher(natsWrapper.client)
+  publisher.publish({
+    id: order.id,
+    ticket: {
+      id: order.ticket.id
+    }
+  })
 
   return res.status(204).send({}) // A 204 response does not include message body
 })
