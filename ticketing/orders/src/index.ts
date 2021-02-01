9d666af6b05b4ead44cfe05ba8bea7fb63f9a9b6
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-01 21:12:10 +0800

    Register ticket listeners when service starts

--- ticketing/orders/src/index.ts
+++ ticketing/orders/src/index.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 import { app } from './app'
 import { natsWrapper } from './nats-wrapper'
 import { DatabaseConnectionError } from '@gittix-js/common'
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener'
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener'
 
 const PORT = 3000
 
@@ -40,6 +42,11 @@ const start = async () => {
     })
     process.on('SIGINT', () => natsWrapper.client.close())
     process.on('SIGTERM', () => natsWrapper.client.close())
+
+    // Listen for ticket creation
+    new TicketCreatedListener(natsWrapper.client).listen()
+    // Listen for ticket update
+    new TicketUpdatedListener(natsWrapper.client).listen()
 
     // Connect to MongoDB instance on service
     await mongoose.connect(process.env.MONGO_URI, {
