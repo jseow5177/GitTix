2988452cf05030d8c0a3935e5f27632a1fab3b73
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-02 12:24:31 +0800

    Add test to ticket model to test OCC

---
+++ ticketing/tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,43 @@
+import { Ticket } from '../ticket'
+
+describe('test ticket model', () => {
+
+  it('correctly implements optimistic concurrency control', async () => {
+    // Create an instance of a ticket
+    const ticket = Ticket.build({
+      title: 'A title',
+      price: 123,
+      userId: '123'
+    })
+
+    // Save the ticket to the database
+    await ticket.save()
+
+    // Fetch two instance of the ticket above from database
+    const firstInstance = await Ticket.findById(ticket.id)
+    const secondInstance = await Ticket.findById(ticket.id)
+
+    // Make two separate changes to the tickets we fetched
+    firstInstance.set({ price: 10 })
+    secondInstance.set({ price: 15 })
+
+    // save the first fetched ticket
+    await firstInstance.save()
+
+    expect(async() => await secondInstance.save()).rejects.toThrow()
+  })
+
+  it('increments version number on multiple saves', async () => {
+    const ticket = Ticket.build({
+      title: 'A title',
+      price: 123,
+      userId: '123'
+    })
+
+    await ticket.save()
+    expect(ticket.version).toEqual(0)
+    await ticket.save()
+    expect(ticket.version).toEqual(1)
+  })
+
+})
