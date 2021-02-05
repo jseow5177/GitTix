a818ae0227bfb80c5918d178b0cb96c483ec0523
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-05 10:00:20 +0800

    Add OrderCancelled listener

---
+++ ticketing/tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,38 @@
+import { Message } from 'node-nats-streaming'
+import { Listener, OrderCancelledEvent, Subjects } from '@gittix-js/common'
+import { queueGroupName } from './queueGroupName'
+import { Ticket } from '../../models/ticket' 
+import { TicketUpdatedPublisher } from '../publishers/ticket-update-publisher'
+
+export class OrderCancelledListener extends Listener <OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled
+  readonly queueGroupName = queueGroupName
+
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+    // Find the ticket that the order is cancelling
+    const ticket = await Ticket.findById(data.ticket.id)
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found')
+    }
+
+    ticket.set({ orderId: undefined })
+
+    // Save the ticket
+    await ticket.save()
+
+    // Emit ticket updated event
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId
+    })
+
+    // Ack the message
+    msg.ack()
+  }
+}
