e8e28d27141d9b1b61a488e27dde6bea6a32b446
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-05 10:00:06 +0800

    Disallow ticket update if ticket has orderId

--- ticketing/tickets/src/routes/update-ticket.ts
+++ ticketing/tickets/src/routes/update-ticket.ts
@@ -4,7 +4,8 @@ import {
   validateRequest,
   NotFoundError,
   requireAuth,
-  NotAuthorizedError
+  NotAuthorizedError,
+  BadRequestError
 } from '@gittix-js/common'
 import { Ticket } from '../models/ticket'
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-update-publisher'
@@ -35,6 +36,10 @@ router.put('/api/tickets/:id',
 
     if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError()
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError('Cannot edit a reserved ticket')
     }
 
     ticket.set({ title, price })
