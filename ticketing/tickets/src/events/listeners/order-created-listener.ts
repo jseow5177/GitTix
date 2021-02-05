cc5aa4469f51a5c8cffce5d15de4a229fd226a46
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-05 10:00:30 +0800

    Add order created listener

---
+++ ticketing/tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,40 @@
+import { Message } from 'node-nats-streaming'
+import { Listener, OrderCreatedEvent, Subjects } from '@gittix-js/common'
+import { queueGroupName } from './queueGroupName'
+import { Ticket } from '../../models/ticket'
+import { TicketUpdatedPublisher } from '../publishers/ticket-update-publisher'
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated
+  readonly queueGroupName = queueGroupName
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id)
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found')
+    }
+
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id })
+
+    // Save the ticket
+    await ticket.save()
+
+    // Emit ticket updated event
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId
+    })
+
+    // Ack the message
+    msg.ack()
+  }
+
+}
