9812c7e89a2e85d4ff895466e5d5576738d7bc64
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-05 10:37:50 +0800

    Init expiration service

---
+++ ticketing/expiration/src/index.ts
@@ -0,0 +1,38 @@
+import { natsWrapper } from './nats-wrapper'
+
+const start = async () => {
+  /**
+   * Ensure that the JWT_KEY is present in the environment variable before starting the application
+  */
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID is not defined')
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL is not defined')
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID is not defined')
+  }
+
+  try {
+    // Connect to NATS
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID, 
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    )
+    // Quit program when NATS shut down
+    natsWrapper.client.on('close', () => {
+      console.log('NATS disconnected!')
+      process.exit()
+    })
+    process.on('SIGINT', () => natsWrapper.client.close())
+    process.on('SIGTERM', () => natsWrapper.client.close())
+
+  } catch (err) {
+    console.log(err)
+  }
+
+}
+
+start()
