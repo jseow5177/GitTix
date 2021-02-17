214373973f841f012a701d8df11cee10d4f4bdd5
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-17 11:13:09 +0800

    Modify home page to view all tickets

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,16 +1,44 @@
-import buildClient from '../services/build-client'
+import Link from 'next/link'
 
-function Home({ currentUser }) {
+function Home({ currentUser, tickets }) {
+
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+    )
+  })
 
   return (
-    <h1>
-      {currentUser ? 'You are signed in' : 'You are not signed in'}
-    </h1>
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ticketList}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
-Home.getInitialProps = async (context) => {
-  return {}
+Home.getInitialProps = async (context, client) => {
+  const { data } = await client.get('/api/tickets')
+
+  return { tickets: data }
 }
 
 export default Home
