58afdbfca26e151faef09c4743241120b2640dd8
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-17 11:13:43 +0800

    Retrieve only unreserved tickets

--- ticketing/tickets/src/routes/get-tickets.ts
+++ ticketing/tickets/src/routes/get-tickets.ts
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/api/tickets', async (req, res) => {
   // Get all tickets
-  const tickets = await Ticket.find({})
+  const tickets = await Ticket.find({ orderId: undefined })
 
   return res.status(200).send(tickets)
 })
